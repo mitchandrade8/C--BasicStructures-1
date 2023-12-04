2330ec467efc274ff7c06dc38fb22ad97fc83f22
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 14:31:20 -0700

    Array Basics Overview in C++

---
+++ Week#1/ArrayBasics.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    /* 
+    int Allan = 71;
+    int Bob = 42;
+    int Carol = 37;
+    int David = 5;
+    int Ellen = 18;
+
+    cout << Allan << endl;
+    */
+
+    int ages[] = {71, 42, 37, 5, 18};
+
+    return 0;
+
+}
