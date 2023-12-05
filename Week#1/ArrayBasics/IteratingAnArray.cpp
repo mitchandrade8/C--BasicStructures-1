7a534afc88a0d88d8eea2d5e0abaa31dedea3671
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 18:17:08 -0700

    IteratingAnArray in C++

---
+++ Week#1/ArrayBasics/IteratingAnArray.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string friends[] = {"Alan", "Bob", "Carol", "David", "Ellen", "Fred", "Grace", "Henry", "Ian", "Jen"};
+
+    cout << friends[0] << endl;
+    cout << friends[1] << endl;
+    cout << friends[2] << endl;
+    cout << friends[3] << endl;
+    cout << friends[4] << endl;
+    cout << friends[5] << endl;
+    cout << friends[6] << endl;
+    cout << friends[7] << endl;
+    cout << friends[8] << endl;
+    cout << friends[9] << endl;
+
+    return 0;
+
+}
